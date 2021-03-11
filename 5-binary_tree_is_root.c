7d5490861c26ecf05998a4e72e41ad2b36319a9c
benhassenwael <greenw2012@gmail.com>
2021-03-11 17:32:38 +0100

    Initial commit

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if node is a root of an entire tree
+ * @node: node to check
+ *
+ * Return: 0 if node is not a root or node is NULL, 1 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node && !(node->parent))
+		return (1);
+	return (0);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measure the height of a binary tree
+ * @tree: root of tree to measure
+ *
+ * Return: integer height of tree or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int lelft_height, right_height;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
